3d96536684c8ca11885608d2594b10440d08b3d5
Aram <Aram.nurjanyan@gmail.com>
2024-09-24 14:45:56 +0300

    Edit Chance base card

--- src/Components/Chance.jsx
+++ src/Components/Chance.jsx
@@ -1,4 +1,54 @@
+function aChance() {
+  return <div style={{
+        padding: '5px',
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: '#f5f5f5',
+        fontSize: "86px",
+        color: 'black'
+  }}>
+    <div style={{
+      height: "100%",
+      width: "100%",
+      border: '1px solid black',
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    }}>
+        <i className="fa-solid fa-question" style={{color: 'black'}}></i>
+    </div>
+  </div>;
+}
+
 function Chance() {
-  return <div></div>;
+  return <div style={{
+        padding: '5px',
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: '#f5f5f5',
+        position: 'relative',
+        color: 'black',
+        fontSize: '24px',
+        wordBreak: 'break-all',
+        overflow: 'hidden',
+  }}>
+    <div style={{
+      height: "100%",
+      width: "100%",
+      border: '1px solid black',
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      position: 'absolute',
+    }}>
+    </div>
+        awfsgfhjfykglh;'o'dadwawfgersyhrtfujtyikyulkhgilgyujrgh
+  </div>;
 }
 export { Chance };
