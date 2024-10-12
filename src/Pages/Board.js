df71a10fd749a25772161a6a8a3b699613201b05
Alfred <afotato0204@gmail.com>
2024-10-12 14:13:12 +0400

    added animated numbers to board

--- src/Pages/Board.js
+++ src/Pages/Board.js
@@ -5,6 +5,8 @@ import Button from "../Components/Button";
 import ShortUniqueId from "short-unique-id";
 import { useRealtimeUpdates, useFetch, updateDB } from "../Hooks/supabase";
 import { Chance } from "../Components/Chance";
+import AnimatedNumbers from "react-animated-numbers";
+
 function Main() {
   const short = new ShortUniqueId({ length: 10 }); //genrates uuid for future
   const uuid = "v6Pstf"; // static uuid
@@ -92,8 +94,15 @@ function Main() {
                     }}
                   ></div>
                 </div>
-                <span>
-                  {name}: {money + "$"}
+                <span style={{ display: "flex" }}>
+                  {name}:
+                  <AnimatedNumbers
+                    transitions={(index) => ({
+                      type: "spring",
+                      duration: index + 0.3,
+                    })}
+                    animateToNumber={money}
+                  />
                 </span>
                 {order == currentOrder && (
                   <i
